216d02aa968a4478724d6acda9d32e38591b910a
PaulBalan1 <56250869+PaulBalan1@users.noreply.github.com>
2020-11-25 18:27:27 +0100

    Made SecretaryTeacher Tier1 functionality

--- SEP3 - TIER1/Gradebook/Data/ISecretaryService.cs
+++ SEP3 - TIER1/Gradebook/Data/ISecretaryService.cs
@@ -1,7 +1,13 @@
+using System.Threading.Tasks;
+using WebApplication.NetworkPackages;
+
 namespace WebApplication.Data
 {
     public interface ISecretaryService
     {
-        
+        Task<NetworkPackage> CreateTeacher(string firstName, string lastName, string password);
+        Task<NetworkPackage> EditTeacher(string id, string password);
+        Task<NetworkPackage> DeleteTeacher(string id);
+        //Task<NetworkPackage> CreateStudent(string firstName, string lastName, string email, string password);
     }
 }
--- SEP3 - TIER1/Gradebook/Data/SecretaryService.cs
+++ SEP3 - TIER1/Gradebook/Data/SecretaryService.cs
@@ -1,7 +1,63 @@
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApplication.NetworkPackages;
+
 namespace WebApplication.Data
 {
     public class SecretaryService : ISecretaryService
     {
-        
+        public async Task<NetworkPackage> CreateTeacher(string firstName, string lastName, string password)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(
+                "http://localhost:" + Startup.PORT + $"/secretary/createteacher?firstName={firstName}&lastName={lastName}&password={password}");
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string userAsJson = await response.Content.ReadAsStringAsync();
+                SecretaryDataPackage resultUser = JsonSerializer.Deserialize<SecretaryDataPackage>(userAsJson);
+                Console.WriteLine(resultUser.ToString());
+                return resultUser;
+            }
+
+            return null;
+        }
+
+        public async Task<NetworkPackage> EditTeacher(string id, string password)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(
+                "http://localhost:" + Startup.PORT + $"/secretary/editteacher?id={id}&password={password}");
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string userAsJson = await response.Content.ReadAsStringAsync();
+                SecretaryDataPackage resultUser = JsonSerializer.Deserialize<SecretaryDataPackage>(userAsJson);
+                Console.WriteLine(resultUser.ToString());
+                return resultUser;
+            }
+
+            return null;
+        }
+
+        public async Task<NetworkPackage> DeleteTeacher(string id)
+        {
+            HttpClient client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(
+                "http://localhost:" + Startup.PORT + $"/secretary/deleteteacher?id={id}");
+
+            if (response.StatusCode == HttpStatusCode.OK)
+            {
+                string userAsJson = await response.Content.ReadAsStringAsync();
+                SecretaryDataPackage resultUser = JsonSerializer.Deserialize<SecretaryDataPackage>(userAsJson);
+                Console.WriteLine(resultUser.ToString());
+                return resultUser;
+            }
+
+            return null;
+        }
     }
 }
